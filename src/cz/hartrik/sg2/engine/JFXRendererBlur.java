87dc4699a04922afa6a255b891f6dbc0242f74a2
Hartrik <patrikharag@gmail.com>
2016-03-22 20:28:26 +0100

    Fix permanent slightly visible shadows caused by motion blur

--- src/cz/hartrik/sg2/engine/JFXRendererBlur.java
+++ src/cz/hartrik/sg2/engine/JFXRendererBlur.java
@@ -1,4 +1,3 @@
-
 package cz.hartrik.sg2.engine;
 
 import cz.hartrik.common.Color;
@@ -8,23 +7,28 @@ import cz.hartrik.sg2.world.element.Air;
 import cz.hartrik.sg2.world.element.FallingElement;
 
 /**
- * Renderer, který trochu rozmazává body. Pouze však elementy dědící z
+ * Renderer, který trochu rozmazává elementy. Pouze však elementy dědící z
  * {@link FallingElement}.
- * 
- * @version 2014-09-27
+ *
+ * @version 2016-03-22
  * @author Patrik Harag
  */
 public class JFXRendererBlur extends JFXRenderer {
 
-    private final float fAlpha    = 0.7f;
-    private final float fAlphaRev = 1.0f - fAlpha;
-    
+    // výchozí průhlednost (1 = neprůhledné)
+    private static final float ALPHA = 0.7f;
+    private static final float WHITE_BACKGROUND = 255 * (1.0f - ALPHA);
+
+    // prvek pole říká, jestli se daný pixel rozmazává
     private final boolean[] blur;
+
+    // prvek pole říká, jestli se při minulém vykreslování na daném místě
+    // nacházel element, který se rozmazává
     private final boolean[] last;
-    
+
     public JFXRendererBlur(ChunkedArea area) {
         super(area);
-        
+
         this.blur = new boolean[area.getWidth() * area.getHeight()];
         this.last = new boolean[area.getWidth() * area.getHeight()];
     }
@@ -33,57 +37,46 @@ public class JFXRendererBlur extends JFXRenderer {
     protected void updateColor(int index) {
         final Element element = area.getArray()[index];
         final Color color = element.getColor();
-        final int dIndex = index * 4;
-        
+        final int i = index * 4;
+
         if (element instanceof Air) {
 
-            if (blur[index]) {
+            if (last[index] && color.equals(Color.WHITE)) {
+                // zahajuje se postupé mizení
 
-                // pokračuje se v mizení
-
-                final int b = data[dIndex]     & 0xFF;
-                final int g = data[dIndex + 1] & 0xFF;
-                final int r = data[dIndex + 2] & 0xFF;
-
-                if (r != 255 && g != 255 && b != 255) {
-                
-                    data[dIndex]     = (byte) ((b * fAlpha) + (color.getBlue()  * fAlphaRev));
-                    data[dIndex + 1] = (byte) ((g * fAlpha) + (color.getGreen() * fAlphaRev));
-                    data[dIndex + 2] = (byte) ((r * fAlpha) + (color.getRed()   * fAlphaRev));
-                
-                    return;
-
-                } else {
-
-                    // mizení ukončeno
-                }
-                
-            } else if (last[index] && color.equals(Color.WHITE)) { // zahajuje se mizení
-                
                 blur[index] = true;
                 last[index] = false;
-                
-                final int b = data[dIndex]     & 0xFF;
-                final int g = data[dIndex + 1] & 0xFF;
-                final int r = data[dIndex + 2] & 0xFF;
+            }
 
-                data[dIndex]     = (byte) ((b * fAlpha) + (color.getBlue()  * fAlphaRev));
-                data[dIndex + 1] = (byte) ((g * fAlpha) + (color.getGreen() * fAlphaRev));
-                data[dIndex + 2] = (byte) ((r * fAlpha) + (color.getRed()   * fAlphaRev));
-                
-                return;
+            if (blur[index]) {
+                // pokračuje se v mizení
+
+                final int b = data[i]     & 0xFF;
+                final int g = data[i + 1] & 0xFF;
+                final int r = data[i + 2] & 0xFF;
+
+                if (isVisible(r, g, b)) {
+                    data[i]     = (byte) ((b * ALPHA) + WHITE_BACKGROUND);
+                    data[i + 1] = (byte) ((g * ALPHA) + WHITE_BACKGROUND);
+                    data[i + 2] = (byte) ((r * ALPHA) + WHITE_BACKGROUND);
+
+                    return;
+                }
             }
         }
-        
-        // žádné mizení | konec mizení
-        
+
+        // bez rozmazání
+
         last[index] = element instanceof FallingElement;
         blur[index] = false;
 
-        data[dIndex]     = color.getByteBlue();
-        data[dIndex + 1] = color.getByteGreen();
-        data[dIndex + 2] = color.getByteRed();
-        
+        data[i]     = color.getByteBlue();
+        data[i + 1] = color.getByteGreen();
+        data[i + 2] = color.getByteRed();
     }
-    
+
+    private boolean isVisible(int r, int g, int b) {
+        return r < 250 && g < 250 && b < 250;
+    }
+
 }

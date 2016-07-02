ecff4bb1f6d55636e4b68b73dc4c87929b7454d1
Hartrik <patrikharag@gmail.com>
2016-07-02 14:04:18 +0200

    Add support for destroyed and glued elements in sgb

--- src/cz/hartrik/sg2/app/sandbox/element/CollectionTest.java
+++ src/cz/hartrik/sg2/app/sandbox/element/CollectionTest.java
@@ -2,17 +2,18 @@
 package cz.hartrik.sg2.app.sandbox.element;
 
 import cz.hartrik.common.io.Resources;
-import cz.hartrik.sg2.brush.Brush;
 import cz.hartrik.sg2.brush.BrushEffect;
 import cz.hartrik.sg2.brush.jfx.BrushCollectionBuilder;
 import cz.hartrik.sg2.brush.manage.BrushManager;
 import cz.hartrik.sg2.world.element.powder.Glueable;
+import cz.hartrik.sg2.world.element.powder.WallPowder;
 import cz.hartrik.sg2.world.element.solid.Destructible;
+import cz.hartrik.sg2.world.element.solid.GluedPowder;
 
 /**
  * Dočasné a testovací štětce.
  *
- * @version 2016-06-15
+ * @version 2016-07-02
  * @author Patrik Harag
  */
 public class CollectionTest {
@@ -24,18 +25,22 @@ public class CollectionTest {
     }
 
     public static void createBrushes(BrushCollectionBuilder collectionBuilder,
-            BrushManager<Brush> manager) {
+            BrushManager manager) {
+
+        BrushEffect brushDestroy = new BrushEffect(
+                collectionBuilder.load(320),
+                e -> (e instanceof Destructible) ? ((Destructible) e).destroy() : null);
+
+        BrushEffect brushGlue = new BrushEffect(
+                collectionBuilder.load(321),
+                e -> (e instanceof Glueable) ? ((Glueable) e).glue() : null);
+
+        brushDestroy.setPredicate(e -> e instanceof WallPowder);
+        brushGlue.setPredicate(e -> e instanceof GluedPowder);
 
         collectionBuilder
-            .add(new BrushEffect(
-                        collectionBuilder.load(320),
-                        e -> (e instanceof Destructible) ? ((Destructible) e).destroy() : null),
-                    URL_WIP)
-
-            .add(new BrushEffect(
-                        collectionBuilder.load(321),
-                        e -> (e instanceof Glueable) ? ((Glueable) e).glue() : null),
-                    URL_WIP)
+            .add(brushDestroy, URL_WIP)
+            .add(brushGlue, URL_WIP)
         ;
     }
 

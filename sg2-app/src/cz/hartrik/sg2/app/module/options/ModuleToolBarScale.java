cacb0168c94e83d3490f1e8c0a308a5da56f396f
Patrik Harag <patrikharag@gmail.com>
2017-07-13 20:48:14 +0200

    Change scale with ctrl + mouse wheel

--- sg2-app/src/cz/hartrik/sg2/app/module/options/ModuleToolBarScale.java
+++ sg2-app/src/cz/hartrik/sg2/app/module/options/ModuleToolBarScale.java
@@ -6,15 +6,17 @@ import cz.hartrik.sg2.app.FrameController;
 import cz.hartrik.sg2.app.Strings;
 import cz.hartrik.sg2.app.module.ApplicationModule;
 import cz.hartrik.sg2.app.service.ServiceManager;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
 import javafx.scene.text.Font;
 
 /**
- * Modul přidá do toolbaru slider pro změnu přiblížení a zaregistruje službu pro
- * reset přiblížení.
+ * Modul přidá do toolbaru slider pro změnu přiblížení, inicializuje změnu
+ * přiblížení po ctrl+scroll a zaregistruje službu pro reset přiblížení.
  *
- * @version 2017-07-05
+ * @version 2017-07-13
  * @author Patrik Harag
  */
 public class ModuleToolBarScale implements ApplicationModule {
@@ -52,9 +54,32 @@ public class ModuleToolBarScale implements ApplicationModule {
         });
 
         manager.register(SERVICE_SCALE_RESET, () -> slider.setValue(1));
+        initOnScroll(app, slider);
 
         controller.getToolBar().getItems()
                 .addAll(lDescription, lScaleFactor, slider);
+    }
+
+    private void initOnScroll(Application app, Slider slider) {
+        Canvas canvas = app.getController().getCanvas().getFxCanvas();
+        ScrollPane scrollPane = app.getController().getCanvas().getScrollPane();
+
+        canvas.setOnScroll(e -> {
+            if (e.isControlDown()) {
+                e.consume();
+
+                // scale
+                double deltaY = e.getDeltaY();
+                if (deltaY < 0)
+                    slider.decrement();
+                else if (deltaY > 0)
+                    slider.increment();
+
+                // center to mouse position
+                scrollPane.setVvalue(e.getY() / canvas.getHeight());
+                scrollPane.setHvalue(e.getX() / canvas.getWidth());
+            }
+        });
     }
 
     protected String format(double value) {
